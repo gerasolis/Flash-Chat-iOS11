5736a95b474a84499075d79270daf0ba2bf229f1
gerasolis <gisolisgr@gmail.com>
2020-06-03 17:23:27 -0500

    TableView methods are added. Now, 3 messages will appear on the ChatView

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // Declare instance variables here
 
@@ -26,11 +26,12 @@ class ChatViewController: UIViewController {
         super.viewDidLoad()
         
         //TODO: Set yourself as the delegate and datasource here:
-        
+        messageTableView.delegate = self
+        messageTableView.dataSource = self
         
         
         //TODO: Set yourself as the delegate of the text field here:
-
+        messageTextfield.delegate = self
         
         
         //TODO: Set the tapGesture here:
@@ -38,30 +39,39 @@ class ChatViewController: UIViewController {
         
 
         //TODO: Register your MessageCell.xib file here:
-
-        
+        messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        configureTableView()
     }
 
     ///////////////////////////////////////////
     
     //MARK: - TableView DataSource Methods
     
-    
-    
     //TODO: Declare cellForRowAtIndexPath here:
-    
-    
+    //Cells that are going to be display on the TableView.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Here, I want to display the Custom Message Cell Design on the TableView.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
+        let messageArray = ["First message", "Second message", "Third message"]
+        cell.messageBody.text = messageArray[indexPath.row]
+        return cell
+    }
     
     //TODO: Declare numberOfRowsInSection here:
-    
-    
+    //Number of rows on the TableView.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
     
     //TODO: Declare tableViewTapped here:
     
     
     
     //TODO: Declare configureTableView here:
-    
+    func configureTableView(){
+        messageTableView.rowHeight = UITableViewAutomaticDimension
+        messageTableView.estimatedRowHeight = 120.0
+    }
     
     
     ///////////////////////////////////////////
