a146541c85d35b229d1593fb2493200bfbd09096
gerasolis <gisolisgr@gmail.com>
2020-06-04 13:21:10 -0500

    SVProgessHUD bar is added after the login action and register action.

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
@@ -29,10 +30,12 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         //TODO: Set up a new user on our Firbase database
+        SVProgressHUD.show()
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print(error!)
             } else {
+                SVProgressHUD.dismiss()
                 print("Registration successful")
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
